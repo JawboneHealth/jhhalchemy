ec7ed9691516d0b8c0d4c97b96b9cbc4b99e9477
RAY <ray@jawbone.com>
2017-09-11 18:01:31 -0400

    JPW-148 Add pypi files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='jhhalchemy',
+    version='0.1',
+    packages=['jhhalchemy', 'jhhalchemy.models'],
+    url='https://bitbucket.aliph.com/projects/JHH/repos/jhhalchemy/browse',
+    license='Apache',
+    author='Ray Courtney',
+    author_email='ray@jawbone.com',
+    description='SQLAlchemy base model and CRUD methods for Jawbone Health'
+)
